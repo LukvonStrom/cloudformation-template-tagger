e1c7fec263a78f9bc22f5b5476297f48e80da071
Lukas <lukas@fruntke.tech>
2020-09-03 19:21:31 +0200

    Add Tag modification logic, improve resource logic

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # cloudformation-template-tagger
+
+## Known Gotchas:
+
+The tool is only able to tag top-level Resources - nested ressources like `AWS::ApiGateway::Deployment StageDescription` are not tagged.
--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 const fs = require('fs').promises;
 const marked = require('marked');
-const path = require('path')
+const path = require('path');
 
 async function main(){
-    let documentation = [];
+    let documentation = {};
     let foundDocumentations = await fs.readdir('../aws-cloudformation-user-guide/doc_source');
-    for(let documentationFile of foundDocumentations.filter(item => item.includes('aws-resource-'))){
-        let rawFileContent = await fs.readFile(path.join('../aws-cloudformation-user-guide/doc_source', documentationFile));
-        let fileContent = marked.lexer(rawFileContent.toString())      
+    for(let documentationFile of foundDocumentations.filter(item => item.includes('aws-resource-') || item.includes('aws-properties-'))){
+        let rawFileContent = await fs.readFile(path.join(__dirname, '../aws-cloudformation-user-guide/doc_source', documentationFile));
+        let fileContent = marked.lexer(rawFileContent.toString()) 
+
+        let resourceName = fileContent[0].tokens[0].text;     
 
         let propertiesIdx = fileContent.findIndex(element=> {
             return element.type === "heading" &&
@@ -20,17 +22,102 @@ async function main(){
 
         let matchingRange = fileContent.slice(propertiesIdx, propertiesIdx + 1 + endIdx);
 
-        let isTaggable = matchingRange.filter(element => {
-            return element.text && element.text.includes("`Tags`")
-        }).length > 0
 
-        let resourceName = fileContent[0].tokens[0].text;
+        let blocklist = JSON.parse(await fs.readFile(path.join(__dirname, "blocklist.json")));
 
+        matchingRange.map(element => {
+            if(element.tokens && element.tokens[0].type === 'codespan' && element.text.includes("Tag")){
 
-        documentation.push({resourceName, isTaggable})
+                let typeOfIndex = element.tokens.findIndex(element => {
+                    return element.type === "em" && element.text === "Type"
+                })
+
+                
+
+                let type = element.tokens[typeOfIndex+1].text;
+
+                if(element.tokens[typeOfIndex+2] && element.tokens[typeOfIndex+2].hasOwnProperty("text")){
+                    type += element.tokens[typeOfIndex+2].text
+                }
+
+                type = type.replace(": ", "").trim();
+
+                if( blocklist.filter(el => element.tokens[0].text.includes(el)).length > 0 || type === "String" || type === "Boolean" || resourceName.includes(" ")){
+
+                    return null;
+                }
+
+                return {resourceName, tagAttribute: element.tokens[0].text, type}
+                
+            }
+        }).filter(el => !!el).forEach((element) => {
+            documentation[`${element.resourceName}-${element.tagAttribute}`] = element;
+        })
+
     }
+
+    // load embedded ressources, match to List of XXX
+    /*
+    let properties = [];
+    
+    for(let documentationFile of foundDocumentations.filter(item => item.includes('aws-properties-'))){
+        let cleanedFileName = (documentationFile.replace("aws-resource-", '')).replace('.md', '');
+        let rawFileContent = await fs.readFile(path.join(__dirname, '../aws-cloudformation-user-guide/doc_source', documentationFile));
+        let fileContent = marked.lexer(rawFileContent.toString()) 
+
+        let propertyName, parsedSyntax;
+        if(fileContent[0].type === "heading" && fileContent[0].depth === 1){
+            propertyName = fileContent[0].tokens[0].text;
+        }
+        let jsonSyntaxEl = fileContent.findIndex(element => element.type === "heading" && element.depth === 3 && element.text.includes("syntax.json") && element.tokens[0].type === "text")
+        if(fileContent[jsonSyntaxEl+1].type === "code"){
+
+        }
+        properties.push({propertyName})
+        await fs.writeFile(path.join(__dirname, 'props/'+ documentationFile + '.json'), JSON.stringify(fileContent, null, 4))
+
+    }*/
+
+    let tags = {
+        Costunit: "ABCDE",
+    }
+
+    let data = JSON.parse(await (await fs.readFile(path.join(__dirname, "../test-cf.json"))).toString())
+
+    for(let [key, resource] of Object.entries(data.Resources)){
+        let foundResource = (Object.entries(documentation)).find(([key, value]) => resource.Type === value.resourceName)
+
+        if(foundResource && foundResource.length > 1 && foundResource[1]){
+            
+            if(resource.Properties.hasOwnProperty(foundResource[1].tagAttribute)){
+                console.log(foundResource[1].tagAttribute)
+                resource.Properties[foundResource[1].tagAttribute] = {...resource.Properties[foundResource[1].tagAttribute], ...tags}
+            }else{
+                resource.Properties[foundResource[1].tagAttribute] = tags;
+            }
+        }else{
+            console.log(resource.Type, foundResource)
+        }
+
+        data[key] = resource;
+        
+    }
+
+    await fs.writeFile(path.join(__dirname, "../test-cf-pre.json"), JSON.stringify(JSON.parse(await (await fs.readFile(path.join(__dirname, "../test-cf.json"))).toString()), null, 4))
+    await fs.writeFile(path.join(__dirname, "../test-cf-post.json"), JSON.stringify(data, null, 4))
+
+
     await fs.writeFile('summary.json', JSON.stringify(documentation, null, 4))
     
+}
+
+function isJsonString(string) {
+    try {
+        JSON.parse(string);
+    } catch (e) {
+        return false;
+    }
+    return true;
 }
 
 main();
